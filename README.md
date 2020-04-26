1a74740d04ccc4914c08d2e1891bd5371b38d4a9
Vincent Serpoul <vincent@serpoul.com>
2020-04-26 12:02:19 +0800

    update usage, versions

--- README.md
+++ README.md
@@ -1,14 +1,69 @@
-WIP
+# Alpine diskless, headless install
+
+These scripts will help you build and write a headless install of alpine linux for SBC.
+
+```
+# Usage: ./run.sh [options]
+#
+# The goal of this script is to create a diskless, headless install of alpine
+# linux for your SBC (rpi, rockpro64...), directly from your x86 computer.
+#
+# Just insert a sdcard and run it with the right parameters.
+#
+# It has a few dependencies: qemu, chroot, parted.
+#
+# Example:
+#   sudo sudo ./run.sh -n myalpine -f
+#
+# Options and environment variables:
+#
+#   -d HARDWARE            which SMB you are targeting.
+#                          Options: rpi
+#                          Default: rpi
+#
+#   -a ARCH                CPU architecture for the SMB.
+#                          Options: x86_64, x86, aarch64, armhf, armv7, ppc64le, s390x.
+#                          Default: aarch64
+#
+#   -m ALPINE_MIRROR...    URI of the Aports mirror to fetch packages from.
+#                          Default: http://dl-cdn.alpinelinux.org/alpine
+#
+#   -b ALPINE_BRANCH       Alpine branch to install.
+#                          Default: latest-stable
+#
+#   -v ALPINE_VERSION      Alpine version to install.
+#                          Default: 3.11.6
+#
+#   -p DEVICE_NAME         Name of the device to write to.
+#                          Default: /dev/sda
+#
+#   -n BUILD_HOSTNAME      Hostname. Must be filled
+#                          No default
+#
+#   -t TIMEZONE            Timezone.
+#                          Default: Asia/Singapore
+#
+#   -f FORCE               If true, don't ask before writing to the device.
+#                          Default: false
+#
+#   -h                     Show this help message and exit.
+#
+# Each option can be also provided by environment variable. If both option and
+# variable is specified and the option accepts only one argument, then the
+# option takes precedence.
+#
+# https://github.com/vincentserpoul/alpine-diskless-headless
+```
 
 # TODO
 
-- [ ] fix apk-cache issue
-- [ ] try to encrypt lbu
+- [ ] encrypt lbu
 - [ ] encrypt secrets (using [age](https://github.com/FiloSottile/age)?)
 - [ ] [ufw](https://wiki.alpinelinux.org/wiki/Uncomplicated_Firewall)
 
 # THANKS
 
+- https://github.com/alpinelinux/alpine-chroot-install
 - https://github.com/knoopx/alpine-raspberry-pi
 - https://github.com/yangxuan8282/gen-rpi_os/blob/master/gen-alpine_rpi.sh
 - https://hütter.ch/posts/pitaya-alpine/#preparations
--- run.sh
+++ run.sh
@@ -1,10 +1,61 @@
 #!/usr/bin/env bash
-
+#---help---
+# Usage: ./run.sh [options]
+#
+# The goal of this script is to create a diskless, headless install of alpine
+# linux for your SBC (rpi, rockpro64...), directly from your x86 computer.
+#
+# Just insert a sdcard and run it with the right parameters.
+#
+# It has a few dependencies: qemu, chroot, parted.
+#
+# Example:
+#   sudo sudo ./run.sh -n myalpine -f
+#
+# Options and environment variables:
+#
+#   -d HARDWARE            which SMB you are targeting.
+#                          Options: rpi
+#                          Default: rpi
+#
+#   -a ARCH                CPU architecture for the SMB.
+#                          Options: x86_64, x86, aarch64, armhf, armv7, ppc64le, s390x.
+#                          Default: aarch64
+#
+#   -m ALPINE_MIRROR...    URI of the Aports mirror to fetch packages from.
+#                          Default: http://dl-cdn.alpinelinux.org/alpine
+#
+#   -b ALPINE_BRANCH       Alpine branch to install.
+#                          Default: latest-stable
+#
+#   -v ALPINE_VERSION      Alpine version to install.
+#                          Default: 3.11.6
+#
+#   -p DEVICE_NAME         Name of the device to write to.
+#                          Default: /dev/sda
+#
+#   -n BUILD_HOSTNAME      Hostname. Must be filled
+#                          No default
+#
+#   -t TIMEZONE            Timezone.
+#                          Default: Asia/Singapore
+#
+#   -f FORCE               If true, don't ask before writing to the device.
+#                          Default: false
+#
+#   -h                     Show this help message and exit.
+#
+# Each option can be also provided by environment variable. If both option and
+# variable is specified and the option accepts only one argument, then the
+# option takes precedence.
+#
+# https://github.com/vincentserpoul/alpine-diskless-headless
+#---help---
 set -euo pipefail
 
 #==============================================================================#
 
-readonly VERSION="0.1.0"
+readonly VERSION="1.0.0"
 
 #============================== i n c l u d e s ===============================#
 
@@ -19,25 +70,26 @@ if [[ ! -d "$DIR_BASE" ]]; then DIR_BASE="$PWD"; fi
 #================================= m a i n ====================================#
 
 while getopts 'd:a:m:b:v:p:n:t:fh' OPTION; do
-	case "$OPTION" in
-	d) HARDWARE="$OPTARG" ;;
-	a) ARCH="$OPTARG" ;;
-	m) ALPINE_MIRROR="$OPTARG" ;;
-	b) ALPINE_BRANCH="$OPTARG" ;;
-	v) ALPINE_VERSION="$OPTARG" ;;
-	p) DEVICE_NAME="$OPTARG" ;;
-	n) BUILD_HOSTNAME="$OPTARG" ;;
-	t) TIMEZONE="$OPTARG" ;;
-	f) FORCE=true ;;
-	h)
-		echo "alpine-diskless-headless-build $VERSION"
-		exit 0
-		;;
-	*)
-		echo "unknown flag"
-		exit 0
-		;;
-	esac
+    case "$OPTION" in
+    d) HARDWARE="$OPTARG" ;;
+    a) ARCH="$OPTARG" ;;
+    m) ALPINE_MIRROR="$OPTARG" ;;
+    b) ALPINE_BRANCH="$OPTARG" ;;
+    v) ALPINE_VERSION="$OPTARG" ;;
+    p) DEVICE_NAME="$OPTARG" ;;
+    n) BUILD_HOSTNAME="$OPTARG" ;;
+    t) TIMEZONE="$OPTARG" ;;
+    f) FORCE=true ;;
+    h)
+        printf "alpine-diskless-headless-run v%s\n\n" "$VERSION"
+        usage
+        exit 0
+        ;;
+    *)
+        echo "unknown flag"
+        exit 0
+        ;;
+    esac
 done
 
 root-check
@@ -46,32 +98,32 @@ helpers-build-hostname-check
 # Set default values
 . """$DIR_BASE""/scripts/defaults.sh"
 
-einfo "running alpine-diskless-headless-build"
+einfo "running alpine-diskless-headless-run"
 
 # apk
 "$DIR_BASE"/apk/build.sh \
-	-a "$ARCH" \
-	-m "$ALPINE_MIRROR" \
-	-b "$ALPINE_BRANCH" \
-	-v "$ALPINE_VERSION" \
-	-n "$BUILD_HOSTNAME" \
-	-t "$TIMEZONE"
+    -a "$ARCH" \
+    -m "$ALPINE_MIRROR" \
+    -b "$ALPINE_BRANCH" \
+    -v "$ALPINE_VERSION" \
+    -n "$BUILD_HOSTNAME" \
+    -t "$TIMEZONE"
 
 # hardware boot
 "$DIR_BASE"/"$HARDWARE"/build.sh \
-	-a "$ARCH" \
-	-m "$ALPINE_MIRROR" \
-	-b "$ALPINE_BRANCH" \
-	-v "$ALPINE_VERSION"
+    -a "$ARCH" \
+    -m "$ALPINE_MIRROR" \
+    -b "$ALPINE_BRANCH" \
+    -v "$ALPINE_VERSION"
 
 # /dev/sda partition, mount, copy files, umount
 if [ "$FORCE" == false ]; then
-	echo
-	read -p "Are you sure you want to format ""$DEVICE_NAME"" (Y/y)?" -n 1 -r
-	echo
-	if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-		die "script stopped by user"
-	fi
+    echo
+    read -p "Are you sure you want to format ""$DEVICE_NAME"" (Y/y)?" -n 1 -r
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        die "script stopped by user"
+    fi
 fi
 
 dev-partition-full "$DEVICE_NAME"
