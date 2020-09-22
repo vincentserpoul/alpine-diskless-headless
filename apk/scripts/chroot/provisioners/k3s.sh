36926edb9603276eece13685a052c0c176e3f1d3
Vincent Serpoul <vincent@serpoul.com>
2020-09-23 00:34:39 +0800

    k3s provisioner updated

--- apk/scripts/chroot/provisioners/k3s.sh
+++ apk/scripts/chroot/provisioners/k3s.sh
@@ -10,20 +10,29 @@ set -eu
 # PROVISIONER_K3S_TOKEN_VALUE: server token
 : "${PROVISIONER_K3S_TOKEN_VALUE?Need to set PROVISIONER_K3S_TOKEN_VALUE}"
 # PROVISIONER_K3S_K3S_ARGS: args for the k3s (agent or server)
-: "${PROVISIONER_K3S_K3S_ARGS?Need to set PROVISIONER_K3S_K3S_ARGS}"
-# PROVISIONER_K3S_TAINTS: taints for the k3s machine
-: "${PROVISIONER_K3S_TAINTS?Need to set PROVISIONER_K3S_TAINTS}"
-# PROVISIONER_K3S_LABELS: labels for the k3s machine
-: "${PROVISIONER_K3S_LABELS?Need to set PROVISIONER_K3S_LABELS}"
+: "${PROVISIONER_K3S_EXEC?Need to set PROVISIONER_K3S_EXEC}"
+# PROVISIONER_K3S_DATA_DIR: datadir
+: "${PROVISIONER_K3S_DATA_DIR?Need to set PROVISIONER_K3S_DATA_DIR}"
 
 #===================================  k 3 s  ==================================#
 
 provisioner_k3s_install_pkg() {
-    apk add k3s
+    apk add curl
+
+    curl -sfL https://get.k3s.io |
+        INSTALL_K3S_EXEC="$PROVISIONER_K3S_EXEC" \
+            sh -s - \
+            --token "$PROVISIONER_K3S_TOKEN_VALUE" \
+            --node-name "$BASE_HOSTNAME" \
+            --node-label "$BASE_ARCH" \
+            --data-dir "$PROVISIONER_K3S_DATA_DIR" || true
+
+    lbu add "$PROVISIONER_K3S_DATA_DIR"
+
 }
 
 #==============================================================================#
 
 printf "\n> k3s setup\n"
 
-privsioner_k3s_install_pkg
+provisioner_k3s_install_pkg
